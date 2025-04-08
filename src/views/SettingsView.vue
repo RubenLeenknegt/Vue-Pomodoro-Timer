59bdd33103ce506e970c758a2efbc16864a7a7b8
Ruben Leenknegt <r.leenknegt@pm.me>
2025-04-08 15:10:37 +0200

    Feat: added content to views

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-
+    <h1 class="font-extrabold text-9xl decoration-red-600 decoration-dashed">This is the SettingsView.vue file!</h1>
 </template>
--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-
+    <h1>This is the TimerView.vue file!</h1>
 </template>
