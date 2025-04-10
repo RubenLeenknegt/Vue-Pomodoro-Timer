28be364c61f9a9620c4c57316d16b11d75ec223e
Ruben Leenknegt <r.leenknegt@pm.me>
2025-04-10 15:17:07 +0200

    Feat: start / stop button functionality

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,47 +1,69 @@
 <script setup>
-    import { ref, defineProps, defineEmits, onMounted } from 'vue'
+import { ref, defineProps, defineEmits, onUnmounted } from 'vue'
 
-    // Define props and convert timerLength into a reactive reference
-    const { timerLength } = defineProps({
-        timerLength: {
-            type: Number,
-            required: true
-        }
-    });
+// Props
+const { timerLength } = defineProps({
+  timerLength: {
+    type: Number,
+    required: true
+  }
+})
 
-    const minutes = ref(timerLength);
-    const seconds = ref(0);
+// State managment
+const minutes = ref(timerLength)
+const seconds = ref(0)
+const timerState = ref(false)
+let interval = null
 
-    const emit = defineEmits(['finished'])
+const emit = defineEmits(['finished'])
+
+// Start the timer
+function startTimer() {
+  if (interval !== null) return // prevent multiple timers from starting
+
+  interval = setInterval(() => {
+    if (seconds.value > 0) {
+      seconds.value--
+    } else if (minutes.value > 0) {
+      minutes.value--
+      seconds.value = 59
+    } else {
+      stopTimer()
+      emit('finished')
+    }
+  }, 1000)
+}
+
+//stops timer 
+function stopTimer() {
+  clearInterval(interval)
+  interval = null
+  timerState.value = false
+}
 
 
-    //Timer logic
+// Toggle timer
+function toggleTimer() {
+  if (timerState.value) {
+    stopTimer()
+  } else {
+    timerState.value = true
+    startTimer()
+  }
+}
 
-    let interval;
-
-
-    function startTimer() {
-        interval = setInterval(() => {
-            if (seconds.value > 0) {
-                seconds.value--;
-            } else if (minutes.value > 0) {
-                minutes.value--;
-                seconds.value = 59;
-            } else {
-                emit('finished');
-            }
-        }, 1000);
-    }   
-
-
-    //starts timer on mount
-    onMounted(() => {
-        startTimer()
-    })
+//Stop timer if user navigates away from page
+onUnmounted(() => {
+  stopTimer()
+})
 </script>
+
 <template>
+  <p class="text-2xl">{{ minutes }} : {{ seconds }}</p>
 
-    <p>{{ minutes }} : {{ seconds }}</p>
-
-
+  <button
+    @click="toggleTimer"
+    class="flex justify-center rounded-md mt-3 bg-green-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 hover:bg-green-600 hover:-translate-y-0.5">
+    {{ timerState ? 'Stop' : 'Start' }}
+  </button>
 </template>
