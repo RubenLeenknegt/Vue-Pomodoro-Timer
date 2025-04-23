05b034c0b167a7f8ad2432162cdcf1aa45c835e0
Ruben Leenknegt <r.leenknegt@pm.me>
2025-04-23 11:04:47 +0200

    refactor: settingsView variable names

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -4,8 +4,8 @@
     const router = useRouter()
 
     //Form variables
-    const workTime = ref(25)
-    const breakTime = ref(10)
+    const workTimeMins = ref(25)
+    const breakTimeMins = ref(10)
     const pomodori = ref(4)
 
     //function to send user and form input to TimerView.vue on form submit
@@ -13,8 +13,8 @@
         router.push({ 
             path: '/timer',
             query: {
-                workTime: workTime.value,
-                breakTime: breakTime.value,
+                workTimeMins: workTimeMins.value,
+                breakTimeMins: breakTimeMins.value,
                 pomodori: pomodori.value
             }
         
@@ -26,24 +26,24 @@
 <template>
     <div class="bg-gray-50 min-h-screen flex items-center justify-center">   
         <div class="bg-white p-8 rounded-2xl shadow-md max-w-2xl w-full text-center">
-            <div class=" max-w-2xl  grid grid-cols-1 justify-items-center">
+            <div>
                 <h2 class="text-5xl font-semibold">Time to focus!</h2>
-                <p class="mt-8 text-lg font-medium sm:text-xl/8 text-center">The Pomodoro Technique is a time management approach designed to increase productivity by building habits and studying in intervals. The key is to be consistent and take regular breaks to improve retention of study material.</p>
+                <p class="mt-8 text-lg font-medium sm:text-xl/8">The Pomodoro Technique is a time management approach designed to increase productivity by building habits and studying in intervals. The key is to be consistent and take regular breaks to improve retention of study material.</p>
             </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form @submit.prevent="form_submit">
-                    <div>
-                        <label class="grid justify-start text-sm/6 font-bold text-gray-900" for="pomodoro-duration">Work time in minutes</label>
+                    <div class="text-gray-900">
+                        <label class="grid justify-start text-sm/6 font-bold" for="pomodoro-duration">Work time in minutes</label>
                         <div>
-                            <input class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" v-model="workTime" type="number" id="pomodoro-duration" min="1" max="60">
+                            <input class="block w-full rounded-md bg-white px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" v-model="workTimeMins" type="number" id="pomodoro-duration" min="1" max="60">
                         </div>
                     </div>
 
                     <div>
                         <label class="grid justify-start text-sm/6 font-bold text-gray-900" for="break-duration">Break time in minutes</label>
                         <div>
-                            <input class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" v-model="breakTime" type="number" id="break-duration" min="1" max="60"> 
+                            <input class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" v-model="breakTimeMins" type="number" id="break-duration" min="1" max="60"> 
                         </div>
                     </div>
 
